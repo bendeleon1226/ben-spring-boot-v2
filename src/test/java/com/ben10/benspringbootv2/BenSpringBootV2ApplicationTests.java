1c57f5423cda579650dd6886ebb97d57f9ef3833
Ben De Leon <bendeleonjr@hotmail.com>
2021-08-22 21:27:19 +0800

    Ben's General Application Using SpringBoot

---
+++ src/test/java/com/ben10/benspringbootv2/BenSpringBootV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ben10.benspringbootv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenSpringBootV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
